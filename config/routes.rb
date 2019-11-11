5d59034995b715bc6d65be056fb62a77bf87c566
garricktgh <garrick@hotmail.sg>
2019-11-11 14:29:15 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :companies
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :companies
+  resources :posts
+  resources :matches
+  resources :messages
 end
