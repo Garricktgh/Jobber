471b7d0e45d4c3eca20d5af4c694bc953ae4d4c1
garricktgh <garrick@hotmail.sg>
2019-11-11 15:36:57 +0800

    add more columns to users table

---
+++ db/migrate/20191111073054_add_more_columns_to_users.rb
@@ -0,0 +1,11 @@
+class AddMoreColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :ddisplay_picture, :text
+    add_column :users, :employment_type, :string
+    add_column :users, :industry, :string
+    add_column :users, :work_experience, :string
+    add_column :users, :education_level, :string
+    add_column :users, :expected_salary, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_070152) do
+ActiveRecord::Schema.define(version: 2019_11_11_073054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,13 @@ ActiveRecord::Schema.define(version: 2019_11_11_070152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.string "name"
+    t.text "ddisplay_picture"
+    t.string "employment_type"
+    t.string "industry"
+    t.string "work_experience"
+    t.string "education_level"
+    t.string "expected_salary"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
