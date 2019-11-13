0eb906fd2577b3fe253dc69a1cae9156153c95ae
Ryan-Yeo <ryanrj@hotmail.sg>
2019-11-13 13:48:56 +0800

    adjust landing page and some buttons for mobile responsiveness

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   EMPLOYMENT_TYPES = %w(Full-time Part-time Contract)
   EDUCATION_LEVELS = %w(PHD Master Bachelor Diploma A-levels ITE O-levels)
-  EXPECTED_SALARIES = %w(100-1000 1000-2000 2000-3000 4000-5000 >5000 >8000 >10000)
+  EXPECTED_SALARIES = %w($100-1k $1k-2k $2k-3k $4k-5k >$5k >$8k >$10k)
   INDUSTRIES = ['Health Care', 'Finance', 'Technology', 'Manufacturing', 'HR', 'Education']
-  WORK_EXPERIENCES = %w(0 1-3 4-6 7-10 >10)
+  WORK_EXPERIENCES = ["0 years", "1-3 years", "4-6 years", "7-10 years", ">10 years"]
 
   belongs_to :company
   has_many :status, :dependent => :delete_all
