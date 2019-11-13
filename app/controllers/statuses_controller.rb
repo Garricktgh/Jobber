1a39c949f036db2223ce5785dd6cd829549284c2
garricktgh <garrick@hotmail.sg>
2019-11-13 18:02:16 +0800

    fix status params

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,17 +1,14 @@
 class StatusesController < ApplicationController
   def create
     @status = Status.find_or_initialize_by(post_id: status_params[:post_id], user_id: status_params[:user_id])
-    p status_params
-    p params[:post_id]
-    p params[:post_approval]
-    if params[:post_approval].present?
-      @status.update_attributes(post_approval: params[:post_approval])
+    if status_params[:post_approval].present?
+      @status.update_attributes(post_approval: status_params[:post_approval])
       @status.save
-      redirect_to post_suggestions(params[:post_id])
+      redirect_to post_suggestions_path(status_params[:post_id])
     elsif params[:user_approval].present?
-      @status.update_attributes(user_approval: params[:user_approval])
+      @status.update_attributes(user_approval: status_params[:user_approval])
       @status.save
-      redirect_to users
+      redirect_to users_path
     end
   end
 
