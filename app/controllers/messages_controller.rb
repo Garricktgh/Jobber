4dc2d59f386e9f1f87b609a0548a1cad723d99bb
Ryan-Yeo <ryanrj@hotmail.sg>
2019-11-14 11:11:32 +0800

    sync views with controller for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,20 @@
 class MessagesController < ApplicationController
+
   def index
     @messages = Message.where()
+
+    if (user_signed_in?)
+      @user = current_user
+      @matches = Status.where(user_id: @user.id, post_approval: "approved", user_approval: "approved")
+      @messages = Message.select('DISTINCT ON ("post_id") *').order(:post_id, created_at: :desc, id: :desc).where(user_id: current_user)
+    elsif (company_signed_in?)
+      @company = current_company
+      @post = Post.where(company_id: current_company.id)
+      @matches = Status.where(post_id: [@post.ids], post_approval: "approved", user_approval: "approved")
+      @messages = Message.select('DISTINCT ON ("user_id") *').order(:user_id, created_at: :desc, id: :desc).where(post_id: [@post.ids])
+    end
+
+
   end
 
   def new
@@ -34,7 +48,7 @@ class MessagesController < ApplicationController
   def destroy
 
   end
-  
+
   private
   def message_params
     params.require(:message).permit(:content)
--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,7 +1,7 @@
 class SuggestionsController < ApplicationController
   def index
     @post = Post.find_by(id: params[:post_id])
-    @statuses = Status.where(post_id: @post).pluck(:user_id)
+    @statuses = Status.where(post_id: @post).where.not(post_approval: "pending").pluck(:user_id)
     @users = User.where(work_experience: @post.work_experience, industry: @post.industry, education_level: @post.education_level, employment_type: @post.employment_type, expected_salary: @post.expected_salary).where.not(id: @statuses)
     @user = @users.first
   end
