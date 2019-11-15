bda96b4870114bc0db5f37466bfa538eb12a7b14
garricktgh <garrick@hotmail.sg>
2019-11-15 15:05:44 +0800

    fix landing page redirect for users and companies

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,12 +1,21 @@
 class LandingPageController < ApplicationController
   def index
 
-    if(company_signed_in?)
+    if company_signed_in?
       @company = current_company
-      redirect_to @company
-    elsif (user_signed_in?)
+      @post = Post.find_by(company_id: @company)
+      if @post.nil?
+        redirect_to edit_company_path(@company)
+      else
+        redirect_to @company
+      end
+    elsif user_signed_in?
       @user = current_user
-      redirect_to @user
+      if @user.employment_type.nil? || @user.industry.nil? || @user.education_level.nil? || @user.expected_salary.nil? || @user.work_experience.nil?
+        redirect_to edit_user_path(@user)
+      else
+        redirect_to users_path
+      end
     end
 
   end
