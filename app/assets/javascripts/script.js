f21f804b999fc7dbae00dfd4a9c0ccc6eea36f12
garricktgh <garrick@hotmail.sg>
2019-11-18 15:25:13 +0800

    clean up codes in script js

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -5,59 +5,61 @@ $(document).ready(function(){
 Paloma.controller('Messages', {
   new: function(){
 
-      //scrolls to the last message
-      let lastChatBox = document.querySelectorAll('.content');
-          lastChatBox = lastChatBox[lastChatBox.length-1];
-          lastChatBox.scrollIntoView();
+  //scrolls to the last message
+  let lastChatBox = document.querySelectorAll('.content');
+  lastChatBox = lastChatBox[lastChatBox.length-1];
+  lastChatBox.scrollIntoView();
 
-            let modals = document.querySelector(".modal")
-            //checks if element is in viewport
-            function elementInViewport2(el) {
-              var top = el.offsetTop;
-              var left = el.offsetLeft;
-              var width = el.offsetWidth;
-              var height = el.offsetHeight;
+  //checks if element is in viewport
+  let modals = document.querySelector(".modal")
+  
+  function elementInViewport2(el) {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
 
-              while(el.offsetParent) {
-                el = el.offsetParent;
-                top += el.offsetTop;
-                left += el.offsetLeft;
-              }
+    while(el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
 
-              return (
-                top < (window.pageYOffset + window.innerHeight) &&
-                left < (window.pageXOffset + window.innerWidth) &&
-                (top + height) > window.pageYOffset &&
-                (left + width) > window.pageXOffset
-              );
-            }
-      //checks if element is scrolled into view
-            function isScrolledIntoView(elem){
-                var docViewTop = $(window).scrollTop();
-                var docViewBottom = docViewTop + $(window).height();
+    return (
+      top < (window.pageYOffset + window.innerHeight) &&
+      left < (window.pageXOffset + window.innerWidth) &&
+      (top + height) > window.pageYOffset &&
+      (left + width) > window.pageXOffset
+    );
+  }
 
-                var elemTop = $(elem).offset().top;
-                var elemBottom = elemTop + $(elem).height();
+  //checks if element is scrolled into view
+  function isScrolledIntoView(elem){
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
 
-                return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-            }
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
 
-          let messageBox = document.querySelector('.msg-text-area')
-          setInterval(() => {
-            if (messageBox.value === "" && elementInViewport2(modals) == false && isScrolledIntoView(lastChatBox) == true) {
-              location.reload();
-            }
-          }, 4000)
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
 
-      messageBox.focus();
-      messageBox.select();
+  //refresh message page periodically
+  let messageBox = document.querySelector('.msg-text-area')
+  setInterval(() => {
+    if (messageBox.value === "" && elementInViewport2(modals) == false && isScrolledIntoView(lastChatBox) == true) {
+      location.reload();
+    }
+  }, 4000)
 
-      document.getElementById("message-box").addEventListener("keyup", (event) => {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            document.querySelector(".message-submit").click();
-        }
-      });
-
+  //select message-text-area
+  messageBox.focus();
+  messageBox.select();
+  
+  $("#message-box").keyup(function(event) {
+      if (event.keyCode === 13) {
+          $(".message-submit").click();
+      }
+    });
   }
 });
